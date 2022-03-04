e1117117832528005c04f473d40ee1243ac2141f
jhon11390 <meljhon2@gmail.com>
2022-03-04 15:17:39 -0500

    se agrega empleadas

--- src/components/empleadas.js
+++ src/components/empleadas.js
@@ -1,6 +1,163 @@
 const Empleadas = () => {
   return ( 
-    <h1>Empleadas</h1>
+    <>
+      <table className="table">
+        <thead>
+        <tr>
+          <th scope="col" className="text-center">Casa</th>
+          <th scope="col">Empleada</th>
+          <th scope="col">Cedula</th>
+          <th scope="col">Modo</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <th scope="row" className="text-center">1</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">2</th>
+          <td>Ana Sanmartin</td>
+          <td>1.049.926.241</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">3</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">4</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">5</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">6</th>
+          <td>Martha Mendoza</td>
+          <td>28.689.448</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">7</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">8</th>
+          <td>Yudis Algarin</td>
+          <td>46.648.086</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">9</th>
+          <td>Alba Perez</td>
+          <td>0</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">10</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>Interna</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">11</th>
+          <td>Ana Palacios</td>
+          <td>35.890.168</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">11</th>
+          <td>Maria</td>
+          <td>0</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">12</th>
+          <td>Sin empleada</td>
+          <td>0</td>
+          <td>No aplica</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">13</th>
+          <td>Yolis Marquez</td>
+          <td>0</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">14</th>
+          <td>Nohora rico</td>
+          <td>36.641.006</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">15</th>
+          <td>Judith Beltran</td>
+          <td>42.208.821</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">16</th>
+          <td>Johana Garcia</td>
+          <td>36.378.629</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">17</th>
+          <td>Ruby Linares</td>
+          <td>52.100.568</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">18</th>
+          <td>Catalina Muñoz</td>
+          <td>39.675.307</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">18</th>
+          <td>Claudia beltran</td>
+          <td>52.420.532</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">19</th>
+          <td>Catalina Muñoz</td>
+          <td>39.675.307</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">20</th>
+          <td>Angelica Pedraza</td>
+          <td>21.019.034</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">21</th>
+          <td>Cielo Garcia</td>
+          <td>36.378.629</td>
+          <td>Por dias</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-center">22</th>
+          <td>Denis Borja</td>
+          <td>45.400.359</td>
+          <td>Por dias</td>
+        </tr>
+        </tbody>
+      </table>
+    </>
    )
 }
  
--- src/components/vehiculos.js
+++ src/components/vehiculos.js
@@ -6,12 +6,12 @@ const Vehiculos = () => {
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingOne">
               <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Vehiculos ordenados por casa
+                <strong>Vehiculos ordenados por casa</strong>
               </button>
             </h2>
             <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                <table className="table small">
+                <table className="table small table-secondary">
                   <thead>
                     <tr>
                       <th scope="col" className="text-center">Casa</th>
@@ -346,12 +346,12 @@ const Vehiculos = () => {
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingTwo">
               <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Vehiculos ordenados alfabeticamente
+                <strong>Vehiculos ordenados alfabeticamente</strong>
               </button>
             </h2>
             <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                <table className="table small">
+                <table className="table small table-striped table-info">
                   <thead>
                     <tr>
                       <th scope="col" className="text-center">Casa</th>
@@ -723,12 +723,377 @@ const Vehiculos = () => {
           <div className="accordion-item">
             <h2 className="accordion-header" id="headingThree">
               <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Vehiculos ordenados numericamente
+                <strong>Vehiculos ordenados numericamente</strong>
               </button>
             </h2>
             <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
               <div className="accordion-body">
-                En proceso :)
+                <table className="table small table-striped table-danger">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="text-center">Casa</th>
+                      <th scope="col">Placa</th>
+                      <th scope="col">Marca</th>
+                      <th scope="col">Modelo</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row" className="text-center">9</th>
+                      <td>003-BTF</td>
+                      <td>MAZDA</td>
+                      <td>ALLEGRO HB</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">10</th>
+                      <td>027-RKV</td>
+                      <td>FORD</td>
+                      <td>ESCAPE XLT</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">5</th>
+                      <td>051-HSR</td>
+                      <td>MAZDA</td>
+                      <td>CX-5</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">7</th>
+                      <td>052-BNT</td>
+                      <td>VW</td>
+                      <td>GOLF</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">1</th>
+                      <td>052-KAL</td>
+                      <td>FORD</td>
+                      <td>ESCAPE XLT</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">14</th>
+                      <td>078-HMS</td>
+                      <td>HYUNDAI</td>
+                      <td>TUCSON</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">15</th>
+                      <td>078-CXQ</td>
+                      <td>VW</td>
+                      <td>JETTA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">6</th>
+                      <td>087-FYS</td>
+                      <td>RENAULT</td>
+                      <td>ZOE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">21</th>
+                      <td>093-EMN</td>
+                      <td>HYUNDAI</td>
+                      <td>SANTA FE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">11</th>
+                      <td>130-ZYW</td>
+                      <td>NISSAN</td>
+                      <td>X-TRAIL</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">12</th>
+                      <td>174-BCB</td>
+                      <td>HONDA</td>
+                      <td>INTEGRA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">14</th>
+                      <td>177-FYT</td>
+                      <td>TOYOTA</td>
+                      <td>HILUX</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">17</th>
+                      <td>179-FZU</td>
+                      <td>MAZDA</td>
+                      <td>3</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">7</th>
+                      <td>227-HSW</td>
+                      <td>NISSAN</td>
+                      <td>MARCH</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">13</th>
+                      <td>262-BJN</td>
+                      <td>CITROEN</td>
+                      <td>SAXO</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">2</th>
+                      <td>267-RLZ</td>
+                      <td>CHEVROLET</td>
+                      <td>CAPTIVA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">20</th>
+                      <td>305-FZY</td>
+                      <td>HONDA</td>
+                      <td>CR-V</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">18</th>
+                      <td>317-GGO</td>
+                      <td>TOYOTA</td>
+                      <td>RAV4</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">15</th>
+                      <td>345-CXQ</td>
+                      <td>MITSUBISHI</td>
+                      <td>MONTERO</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">6</th>
+                      <td>347-EJV</td>
+                      <td>CHEVROLET</td>
+                      <td>EQUINOX</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">8</th>
+                      <td>384-BRM</td>
+                      <td>CHEVROLET</td>
+                      <td>TRAILBLAZER</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">2</th>
+                      <td>406-JWX</td>
+                      <td>TOYOTA</td>
+                      <td>CROSS</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">1</th>
+                      <td>407-IYN</td>
+                      <td>FORD</td>
+                      <td>LIMITED</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">7</th>
+                      <td>410-UBX</td>
+                      <td>VW</td>
+                      <td>GOLF</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">6</th>
+                      <td>424-GMY</td>
+                      <td>VOLVO</td>
+                      <td>V40 T4</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">4</th>
+                      <td>443-CYD</td>
+                      <td>FORD</td>
+                      <td>ESCAPE XLT</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">20</th>
+                      <td>455-FPO</td>
+                      <td>AUDI</td>
+                      <td>AUDI</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">8</th>
+                      <td>470-BMP</td>
+                      <td>TOYOTA</td>
+                      <td>4RUNNER</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">10</th>
+                      <td>480-UBO</td>
+                      <td>FORD</td>
+                      <td>ESCAPE SE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">4</th>
+                      <td>508-NCW</td>
+                      <td>CHEVROLET</td>
+                      <td>SPARK GT</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">19</th>
+                      <td>520-DDD</td>
+                      <td>BMW</td>
+                      <td>ESCAPE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">7</th>
+                      <td>529-CZN</td>
+                      <td>VW</td>
+                      <td>JETTA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">20</th>
+                      <td>530-BOP</td>
+                      <td>HONDA</td>
+                      <td>CIVIC</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">2</th>
+                      <td>540-HTK</td>
+                      <td>MG</td>
+                      <td>MG3</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">22</th>
+                      <td>549-DDR</td>
+                      <td>BMW</td>
+                      <td>320i</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">10</th>
+                      <td>550-DMZ</td>
+                      <td>MAZDA</td>
+                      <td>CX-5</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">18</th>
+                      <td>562-CXZ</td>
+                      <td>RENAULT</td>
+                      <td>CLIO</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">5</th>
+                      <td>565-EJO</td>
+                      <td>MAZDA</td>
+                      <td>CX-9</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">14</th>
+                      <td>596-ENU</td>
+                      <td>TOYOTA</td>
+                      <td>RAV4</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">11</th>
+                      <td>625-MPX</td>
+                      <td>FORD</td>
+                      <td>FIESTA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">5</th>
+                      <td>628-RNT</td>
+                      <td>AUDI</td>
+                      <td>A4</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">15</th>
+                      <td>645-DOZ</td>
+                      <td>BMW</td>
+                      <td>X3</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">9</th>
+                      <td>670-BHN</td>
+                      <td>HYUNDAI</td>
+                      <td>ELANTRA</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">13</th>
+                      <td>673-FZX</td>
+                      <td>CHEVROLET</td>
+                      <td>BEAT PREMIER</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">14</th>
+                      <td>681-IWY</td>
+                      <td>FORD</td>
+                      <td>ESCAPE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">16</th>
+                      <td>707-QFQ</td>
+                      <td>TOYOTA</td>
+                      <td>FORTUNER</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">22</th>
+                      <td>744-URR</td>
+                      <td>KIA</td>
+                      <td>RIO SPICE</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">5</th>
+                      <td>762-FPV</td>
+                      <td>MINI-COOPER</td>
+                      <td>COOPER-S</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">21</th>
+                      <td>790-IDV</td>
+                      <td>RENAULT</td>
+                      <td>SANDERO</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">16</th>
+                      <td>802-BNW</td>
+                      <td>RENAULT</td>
+                      <td>SCENIC</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">9</th>
+                      <td>804-JDM</td>
+                      <td>NISSAN</td>
+                      <td>MARCH</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">21</th>
+                      <td>816-HIZ</td>
+                      <td>BMW</td>
+                      <td>X5</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">4</th>
+                      <td>828-CVT</td>
+                      <td>RENAULT</td>
+                      <td>LOGAN</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">12</th>
+                      <td>857-RZO</td>
+                      <td>HONDA</td>
+                      <td>CR-V</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">10</th>
+                      <td>886-FNW</td>
+                      <td>RENAULT</td>
+                      <td>DUSTER</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">15</th>
+                      <td>904-BTK</td>
+                      <td>JEEP</td>
+                      <td>CHEROKEE CRD</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">17</th>
+                      <td>964-UGW</td>
+                      <td>MAZDA</td>
+                      <td>CX-5</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">19</th>
+                      <td>981-ARD</td>
+                      <td>BMW</td>
+                      <td>323i</td>
+                    </tr>
+                    <tr>
+                      <th scope="row" className="text-center">3</th>
+                      <td>No aplica</td>
+                      <td>No aplica</td>
+                      <td>No aplica</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
